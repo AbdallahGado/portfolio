43756dd651bd3f1a4083d0e202eca37716bc14f8
AbdallahGado <abdallahgado22@gmail.com>
2024-09-12 20:00:06 +0300

    added SpeedInsights

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { navItems } from "@/data";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
@@ -14,6 +14,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <SpeedInsights />
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
